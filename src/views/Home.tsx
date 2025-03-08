6548a55819573424eb1b392ff4cbb4e4082c070d
Sokorinjo <vsokorac65@gmail.com>
2025-03-08 14:32:14 +0100

    Remove unused imports

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { type Post } from '../lib/types.ts'
 import ShowPostsTable from '../components/Table.tsx'
 import NewItemForm from '../components/NewItemForm.tsx'
-import { PostsContext } from '../context/postsContext.ts'
 
 export default function Home() {
   const [filteredPosts, setFilteredPosts] = useState<Post[]>([])
 
   //Fetch posts from URL
-  const {isLoading, isError, isSuccess} = useQuery({
+  const { isLoading, isError, isSuccess } = useQuery({
     queryKey: ['posts'],
     queryFn: async () => {
       const response = await fetch('http://jsonplaceholder.typicode.com/posts')
@@ -19,24 +18,25 @@ export default function Home() {
       })
       setFilteredPosts(filteredPosts)
       return filteredPosts
-  }})
+    }
+  })
 
   //Check if loading
   if (isLoading) return <div>Loading...</div>
-  
+
   //Check if error occured
   if (isError) return <div>Error has occured</div>
 
-  if(isSuccess) {
+  if (isSuccess) {
     return (
-    <>
+      <>
         <h1 className=''>Home</h1>
         <p></p>
         <div>
-          <PostsContext.Provider value={{filteredPosts, setFilteredPosts}}>
-            <NewItemForm filteredPosts={filteredPosts} setFilteredPosts={setFilteredPosts}/>
-            <ShowPostsTable filteredPosts={filteredPosts} setFilteredPosts={setFilteredPosts}/>
-          </PostsContext.Provider>
+          <div className='flex justify-between'>
+            <NewItemForm filteredPosts={filteredPosts} setFilteredPosts={setFilteredPosts} />
+          </div>
+          <ShowPostsTable filteredPosts={filteredPosts} setFilteredPosts={setFilteredPosts} />
         </div>
       </>
     )
